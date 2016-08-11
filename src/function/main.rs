c1b87dd7241924078a5d8be03822090bc9dd5d2f
Fengming Sun <sunfengming@meituan.com>
2016-08-11 16:18:41 +0800

    add function demo

---
+++ src/function/main.rs
@@ -0,0 +1,37 @@
+fn factory() -> Box<Fn(i32) -> i32> {
+    let num = 5;
+    Box::new(move |x| x + num)
+}
+
+
+trait Foo {
+    fn f(&self);
+}
+
+trait Bar {
+    fn f(&self);
+}
+
+struct Baz;
+
+impl Foo for Baz {
+    fn f(&self) {
+        println!("Baz's impl of Foo");
+    }
+}
+
+impl Bar for Baz {
+    fn f(&self) {
+        println!("Baz's impl of Bar");
+    }
+}
+
+fn main() {
+    let f = factory();
+    let answer = f(1);
+    assert_eq!(6, answer);
+
+    let b = Baz;
+    Bar::f(&b);
+    Foo::f(&b);
+}

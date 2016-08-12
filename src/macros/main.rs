a55fd725c630f19b237af7786a5fe5e35f32944e
Fengming Sun <sunfengming@meituan.com>
2016-08-12 15:28:33 +0800

    add macros demo

---
+++ src/macros/main.rs
@@ -0,0 +1,24 @@
+#![allow(unused_must_use)]
+macro_rules! write_html {
+    ($w:expr, ) => (());
+    ($w:expr, $e:tt) => (write!($w, "{}", $e));
+    ($w:expr, $tag:ident [ $($inner:tt)* ] $($rest:tt)*) => {{
+        write!($w, "<{}>", stringify!($tag));
+        write_html!($w, $($inner)*);
+        write!($w, "</{}>", stringify!($tag));
+        write_html!($w, $($rest)*);
+    }};
+}
+
+pub fn main() {
+    use std::fmt::Write;
+    let mut out = String::new();
+
+    write_html!(&mut out,
+        html[
+            head[title["Macros guide"]]
+            body[h1["Macros are the best!"]]
+        ]);
+
+    println!("{}", out);
+}
